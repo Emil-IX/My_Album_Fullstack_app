0244344b798c858767f7d7f6da5da3534462b893
Emil-Spi <xiriusix@gmail.com>
2025-10-02 16:43:19 -0400

    v1.8 Update modal

--- frontend/src/pages/UsersManager.jsx
+++ frontend/src/pages/UsersManager.jsx
@@ -1,13 +1,23 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 import api from "../api/axios";
 import { useAuth } from "../context/AuthContext";
-import { Edit,  Trash2} from "lucide-react"
+import { Edit, Trash2 } from "lucide-react"
 
 export default function UsersManager() {
   const { token, loadingAuth } = useAuth();
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+
+  const [appearModal, setAppearModal] = useState(false)
+  const [selectUser, setSelectUser] = useState(null)
+  const [formInformation, setFormInformation] = useState({
+    name: '',
+    role: '',
+    age: '',
+    email: '',
+  })
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -34,18 +44,51 @@ export default function UsersManager() {
   }, [token, loadingAuth]);
 
 
-  const deleteUser  = async (userId) => {
+  const deleteUser = async (userId) => {
     try {
       let sure = confirm("Are you sure you want errase this user?")
-      if(sure){
+      if (sure) {
         const res = await api.delete(`/users/${userId}`)
-         setData((prev) => prev.filter((u) => u._id !== userId ) )
+        setData((prev) => prev.filter((u) => u._id !== userId))
       }
     } catch (err) {
       setError(err.response?.data?.message || err.message)
     }
   }
 
+
+  const openEditModal = (user) => {
+    setSelectUser(user)
+    setFormInformation({
+      name: user.name,
+      role: user.role,
+      age: user.age,
+      email: user.email,
+    })
+    setAppearModal(true)
+  }
+
+  const onChangeInputs = (e) => {
+    setFormInformation({
+      ...formInformation,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const updateUser = async (e) => {
+    e.preventDefault()
+
+    try {
+      const res = await api.put(`/users/${selectUser._id}`, formInformation)
+      setData((prev) =>
+        prev.map(user => user._id == selectUser._id ? res.data : user)
+      )
+      setAppearModal(false)
+    } catch (err) {
+      setError(err.response?.data?.message || err.message)
+    }
+
+  }
 
   if (loading) return <p>Loading users...</p>;
   if (error) return <p>Error: {error}</p>;
@@ -76,19 +119,19 @@ export default function UsersManager() {
               <td className="px-6 py-4 text-sm text-gray-600">
                 {age.split('T')[0]}</td>
               <td className="px-6 py-4 text-center flex justify-center gap-3">
-                 <button
-                className="p-2 bg-blue-100 text-blue-600 rounded-full hover:bg-blue-200"
-                onClick={() => console.log("Edit", _id)}
-              >
-                <Edit className="w-5 h-5" />
-              </button>
+                <button
+                  className="p-2 bg-blue-100 text-blue-600 rounded-full hover:bg-blue-200"
+                  onClick={() => openEditModal({ _id, name, role, age, email })}
+                >
+                  <Edit className="w-5 h-5" />
+                </button>
 
-              <button
-                className="p-2 bg-red-100 text-red-600 rounded-full hover:bg-red-200"
-                onClick={() => deleteUser(_id)}
-              >
-                <Trash2 className="w-5 h-5" />
-              </button>
+                <button
+                  className="p-2 bg-red-100 text-red-600 rounded-full hover:bg-red-200"
+                  onClick={() => deleteUser(_id)}
+                >
+                  <Trash2 className="w-5 h-5" />
+                </button>
 
               </td>
             </tr>
@@ -97,6 +140,66 @@ export default function UsersManager() {
         </tbody>
 
       </table>
+      {/*  Modal */}
+      {appearModal && (
+        <div className="">
+          <div className="">
+            <h3>Edit User</h3>
+            <form onSubmit={updateUser}>
+              <input
+                type="text"
+                name="name"
+                value={formInformation.name}
+                onChange={onChangeInputs}
+                placeholder="Name"
+                className="w-full p-2 border rounded"
+              />
+              <input
+                type="text"
+                name="role"
+                value={formInformation.role}
+                onChange={onChangeInputs}
+                placeholder="Role"
+                className="w-full p-2 border rounded"
+              />
+              <input
+                type="date"
+                name="age"
+                value={formInformation.age}
+                onChange={onChangeInputs}
+                className="w-full p-2 border rounded"
+              />
+              <input
+                type="email"
+                name="email"
+                value={formInformation.email}
+                onChange={onChangeInputs}
+                placeholder="Email"
+                className="w-full p-2 border rounded"
+                disabled={true}
+              />
+
+              <div>
+                <button
+                  type="button"
+                  onClick={() => setAppearModal(false)}
+                >
+                  Cancel
+                </button>
+                <button
+                  type="submit"
+                  className=""
+                >
+                  Update
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+
+      )}
+
+
 
     </div>
   );
